c1414a82df7bb1ad7a409a5e31e374ef411d3463
envygod-dev <mbsavage0930@gmail.com>
2024-10-05 13:25:20 +0800

    updated queries

--- db/query/productMn.sql
+++ db/query/productMn.sql
@@ -1,53 +1,63 @@
--- -- name: CreateProductMn :one
--- INSERT INTO
---     "productMn" (
---         "CategoryMnID",
---         "PriceMn",
---         "StockQuantity",
---         "ImagesPathMn"
---     )
--- VALUES
---     (
---         "CategoryMnID" - sqlc.arg ('CategoryMnID'),
---         "PriceMn" = sqlc.arg ('PriceMn'),
---         "StockQuantity" = sqlc.arg ('StockQuantity'),
---         "ImagesPathMn" = sqlc.arg ('ImagesPathMn')
---     ) RETURNING *;
--- -- name: GetListProductMn :many
--- SELECT
---     *
--- FROM
---     "productMn"
--- ORDER BY
---     "Created_At" DESC;
--- -- name: UpdateByProductMnPrice :one
--- UPDATE "productMn"
--- SET
---     "PriceMn" = sqlc.arg ('PriceMn')
--- WHERE
---     "ProductMnID" = sqlc.arg ('ProductMnID') RETURNING *;
--- -- name: UpdateByProductMnStockQuantity :one
--- UPDATE "productMn"
--- SET
---     "StockQuantity" = sqlc.arg ('StockQuantity')
--- WHERE
---     "ProductMnID" = sqlc.arg ('ProductMnID') RETURNING *;
--- -- name: UpdateByMnImagePath :one
--- UPDATE "productMn"
--- SET
---     "ImagesPathMn" = sqlc.arg ('ImagesPathMn')
--- WHERE
---     "ProductMnID" = sqlc.arg ('ProductMnID') RETURNING *;
--- -- name: DeleteByProductMnId :exec
--- DELETE FROM "productMn"
--- WHERE
---     "ProductMnID" = sqlc.arg ('ProductMnID');
--- -- name: FilterByProductMnName :many
--- SELECT
---     *
--- FROM
---     "productMn"
--- WHERE
---     "ProductMnName" ILIKE '%' || sqlc.arg ('ProductMnName') || '%' -- Case-insensitive search for partial match
--- ORDER BY
---     "Created_At" DESC;
+-- name: CreateProductMn :one
+INSERT INTO
+    "productMn" (
+        "ProductNameMn",
+        "ImageID",
+        "subCategoryIDMn",
+        "PriceMn",
+        "StockQuantity",
+        "ImagesPathMn"
+    )
+VALUES
+    (
+        "ProductNameMn" = sqlc.arg('ProductNameMn'),
+        "ImageID" = sqlc.arg('ImageID'),
+        "subCategoryIDMn" = sqlc.arg('subCategoryIDMn'),
+        "PriceMn" = sqlc.arg('PriceMn'),
+        "StockQuantity" = sqlc.arg('StockQuantity'),
+        "ImagesPathMn" = sqlc.arg('ImagesPathMn')
+    ) RETURNING *;
+
+-- name: GetListProductMn :many
+SELECT
+    *
+FROM
+    "productMn"
+ORDER BY
+    "Created_At" DESC;
+
+-- name: UpdateByProductMnPrice :one
+UPDATE "productMn"
+SET
+    "PriceMn" = sqlc.arg ('PriceMn')
+WHERE
+    "ProductMnID" = sqlc.arg ('ProductMnID') RETURNING *;
+    
+-- name: UpdateByProductMnStockQuantity :one
+UPDATE "productMn"
+SET
+    "StockQuantity" = sqlc.arg ('StockQuantity')
+WHERE
+    "ProductMnID" = sqlc.arg ('ProductMnID') RETURNING *;
+
+-- name: UpdateByMnImagePath :one
+UPDATE "productMn"
+SET
+    "ImagesPathMn" = sqlc.arg ('ImagesPathMn')
+WHERE
+    "ProductMnID" = sqlc.arg ('ProductMnID') RETURNING *;
+
+-- name: DeleteByProductMnId :exec
+DELETE FROM "productMn"
+WHERE
+
+    "ProductMnID" = sqlc.arg ('ProductMnID');
+-- name: FilterByProductMnName :many
+SELECT
+    *
+FROM
+    "productMn"
+WHERE
+    "ProductMnName" ILIKE '%' || sqlc.arg ('ProductMnName') || '%' -- Case-insensitive search for partial match
+ORDER BY
+    "Created_At" DESC;
