5d2e112fca2547c57cea0fed12afdf2fc19dfc50
envygod-dev <mbsavage0930@gmail.com>
2024-10-07 21:12:47 +0800

    detail models done

--- models/detail.go
+++ models/detail.go
@@ -1,8 +1,27 @@
 package models
 
+type DetailEn struct {
+	DetailEnID  int32  `json:"detail_en_id"`
+	ProductEnID int32  `json:"product_en_id"`
+	ChoiceName  string `json:"choice_name"`
+	ChoiceValue string `json:"choice_value"`
+}
+
 type CreateDetailEnRequest struct {
-	DetailEnID  int32  `json:"detailEnID"`
-	ProductEnID int32  `json:"productEnID"`
-	ChoiceName  string `json:"choiceName"`
-	ChoiceValue string `json:"choiceValue"`
+	ProductEnID int32  `json:"product_en_id" validate:"required"`
+	ChoiceName  string `json:"choice_name" validate:"required"`
+	ChoiceValue string `json:"choice_value" validate:"required"`
+}
+
+type UpdateDetailEnRequest struct {
+	DetailEnID  int32  `json:"detail_en_id" validate:"required"`
+	ChoiceName  string `json:"choice_name" validate:"required"`
+	ChoiceValue string `json:"choice_value" validate:"required"`
+}
+
+type DetailEnResponse struct {
+	DetailEnID  int32  `json:"detail_en_id"`
+	ProductEnID int32  `json:"product_en_id"`
+	ChoiceName  string `json:"choice_name"`
+	ChoiceValue string `json:"choice_value"`
 }
